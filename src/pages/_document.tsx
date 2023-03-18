557bd6ef76f2396eb63c80ed641f8dedeb5be2cd
niwatoro <girlwithskyhairpin@gmail.com>
2023-03-18 18:30:46 +0900

    logo updated

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/logo.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={""} />
         <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond&family=Gulzar&family=Poppins&family=Russo+One&family=Sawarabi+Gothic&family=ZCOOL+QingKe+HuangYou&family=ZCOOL+XiaoWei&family=Zen+Antique&family=Noto+Sans+JP&family=Press+Start+2P&display=swap" rel="stylesheet" />
